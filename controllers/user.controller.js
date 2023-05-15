778e083444c9e54b62c439c2e1076d0f84cbe722
Thibault Gilbin <zaideby@gmail.com>
2023-05-15 02:37:27 +0200

    fix nodejs callbacks & add user-crud oauth2-authent

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,12 +7,122 @@ module.exports.getAllUsers = async (req, res) => {
 }
 
 module.exports.userInfo = async (req, res) => {
-    console.log(req.params); //req.params = paramètre mis dans l'url
+    console.log(req.params); // req.params = parameter passed in the URL
     if (!ObjectID.isValid(req.params.id))
-        return res.status(400).send('ID unknown : ' + req.params.id)
-    
-    UserModel.findById.exec(req.params.id, (err, docs) => {
-        if (!err) res.send(docs);
-        else console.log('ID unknown : ' + err);
-    }).select('-password')
-}
+      return res.status(400).send('ID unknown: ' + req.params.id);
+  
+    try {
+      const docs = await UserModel.findById(req.params.id).select('-password');
+      res.send(docs);
+    } catch (err) {
+      console.log('ID unknown: ' + err);
+      res.status(500).send('Internal Server Error');
+    }
+};
+
+module.exports.updateUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send('ID unknown: ' + req.params.id);
+  
+    try {
+      const docs = await UserModel.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $set: {
+            bio: req.body.bio,
+          },
+        },
+        { new: true, upsert: true, setDefaultsOnInsert: true }
+      );
+  
+      if (!docs) {
+        return res.status(404).send('User not found');
+      }
+  
+      return res.send(docs);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: err });
+    }
+};
+  
+module.exports.deleteUser = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send('ID unknown: ' + req.params.id);
+  
+    try {
+      const result = await UserModel.deleteOne({ _id: req.params.id });
+      if (result.deletedCount === 0) {
+        return res.status(404).send('User not found');
+      }
+      return res.status(200).json({ message: 'Successfully deleted' });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: err });
+    }
+};
+  
+module.exports.follow = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send('ID unknown: ' + req.params.id);
+  
+    try {
+      // Add to the follower list
+      const user = await UserModel.findByIdAndUpdate(
+        req.params.id,
+        { $addToSet: { following: req.body.idToFollow } },
+        { new: true, upsert: true }
+      );
+  
+      if (!user) {
+        return res.status(404).send('User not found');
+      }
+  
+      // Add to the following list
+      await UserModel.findByIdAndUpdate(
+        req.body.idToFollow,
+        { $addToSet: { followers: req.params.id } },
+        { new: true, upsert: true }
+      );
+  
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: err });
+    }
+};
+  
+  
+module.exports.unfollow = async (req, res) => {
+    if (
+      !ObjectID.isValid(req.params.id) ||
+      !ObjectID.isValid(req.body.idToUnfollow)
+    )
+      return res.status(400).send("ID unknown : " + req.params.id);
+  
+    try {
+      // Remove from the follower list
+      const user = await UserModel.findByIdAndUpdate(
+        req.params.id,
+        { $pull: { following: req.body.idToUnfollow } },
+        { new: true, upsert: true }
+      );
+  
+      if (!user) {
+        return res.status(404).send('User not found');
+      }
+  
+      // Remove from the following list
+      await UserModel.findByIdAndUpdate(
+        req.body.idToUnfollow,
+        { $pull: { followers: req.params.id } },
+        { new: true, upsert: true }
+      );
+  
+      res.status(201).json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: err });
+    }
+};
+  
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,12 +4,15 @@ const userController = require('../controllers/user.controller');
 
 //auth
 router.post("/register", authController.signUp);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
 
 //user modifications in the DB
 router.get('/', userController.getAllUsers)
 router.get('/:id', userController.userInfo)
-
-
-
+router.put("/:id", userController.updateUser);
+router.delete("/:id", userController.deleteUser);
+router.patch("/follow/:id", userController.follow);
+router.patch("/unfollow/:id", userController.unfollow);
 
 module.exports = router;
